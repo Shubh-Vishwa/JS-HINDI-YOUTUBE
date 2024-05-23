b6895b8ba1bfc520ec6faac8929efa65658d99b5
Shubham Vishwakarma <84528200+Shubh-Vishwa@users.noreply.github.com>
2024-05-23 18:40:07 +0000

    done for the data types and variables

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,41 @@
+// "use strict"; // Treat all JS code as a newer version .
+// alert(3+3); //We are using Node js not a browser so we can not test right now as alert.
+a=8
+console.log(a);
+
+/**
+ * impact of the use strict for undeclared variable in previous version works fine,
+ * But when we write this after the  use of "use strict" it throws error  it means 
+ * what ever the code you have written will now work according to the new version of js .
+ */
+
+
+let name = "shubham"
+let age = 23
+let isloggin = true
+
+// PRIMITIVE DATATYPE 
+                    //number => 2^53   ie ramnge 
+                    //bigint  // we dont use often such a large number ie  used in trading 
+                    // string => "" or '' but prefer double string 
+                    // boolean => true / false
+                    //null  => standalone value 
+                    // undefined =>  abhi value assign nahi hui hai 
+                    // symabol => They are used to represent unique values that 
+                    // can be used as identifiers or keys in objects.
+
+// NOTE : When adding a number and a string, JavaScript will treat the number as a string.
+// let  str= 23+"shubhu"
+// console.log(str);
+
+// DatatType Check
+// 1> typeof => this is basically used for checking the datatype 
+
+console.log(typeof undefined);//undefined basically khud me hi type hai 
+// output:undefined 
+console.log(typeof null); // null  is basically a object 
+//output : object
+
+
+
+
---
+++ 01_basics/o1_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553
+let accountEmail = "shubhamon30@gmail.com"
+var accountPassword = "12345"
+let accountState
+accountCity = "Raebareli"
+
+// accountId = 1223321 // not allowed in js 
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword]);
+
+accountEmail = "shubhu@gmail.com"
+accountPassword = "7830327"
+console.table([accountEmail,accountId,accountPassword,accountState]);
+
+/*
+Prefer not to use the  var because of its block scope and functional scope.
+ */
+
+/**
+ * In this we make the use of only let and const .
+ * we also avoid the use of undeclared variable insted  of its working but we avoid the use of undeclared variable .
+ */
+
+
+
+
